e915a650d90af8563aaa40148c62b9ed083aaa0f
U-keun Song <sukeun319@gmail.com>
2023-05-24 21:36:36 +0900

    Optional to Mono : Optional을 이용해서 Mono에 데이터 넣기

---
+++ src/main/java/com/example/reactiveexample/OptionalToMono.java
@@ -0,0 +1,16 @@
+package com.example.reactiveexample;
+
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+public class OptionalToMono {
+    public static void main(String[] args) {
+        Optional<String> optional = Optional.of("우근");
+
+        Mono<String> stringMono = Mono.justOrEmpty(optional).log();
+
+        stringMono.subscribe(System.out::println);
+    }
+
+}
