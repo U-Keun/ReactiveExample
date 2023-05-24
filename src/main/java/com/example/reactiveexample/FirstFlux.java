6f3c3cb4787a84f06fb676348155eb9650381467
U-keun Song <sukeun319@gmail.com>
2023-05-24 20:36:37 +0900

    Flux에 데이터를 추가하는 방법(2)

--- src/main/java/com/example/reactiveexample/FirstFlux.java
+++ src/main/java/com/example/reactiveexample/FirstFlux.java
@@ -5,15 +5,17 @@ import reactor.core.publisher.Flux;
 public class FirstFlux {
     public static void main(String[] args) {
         // Flux는 publisher 역할
-        Flux<String> stringFlux = Flux.just("우근", "현진", "승연", "정아");
+        Flux<String> stringFlux = Flux.just("우근", "현진", "승연", "정아")
+                .concatWithValues("16조")
+                .concatWith(Flux.just("챌린지팀"));
 
-        // Flux에 데이터를 직접추가
-        stringFlux = stringFlux.concatWithValues("16조");
-
-        Flux<String> secondFlux = Flux.just("챌린지팀");
-
-        // Flux에 다른 Flux 데이터 추가
-        stringFlux = stringFlux.concatWith(secondFlux);
+//        // Flux에 데이터를 직접추가
+//        stringFlux = stringFlux.concatWithValues("16조");
+//
+//        Flux<String> secondFlux = Flux.just("챌린지팀");
+//
+//        // Flux에 다른 Flux 데이터 추가
+//        stringFlux = stringFlux.concatWith(secondFlux);
 
         // Flux에 있는 데이터를 출력하는 방법
         stringFlux.subscribe(System.out::println);
