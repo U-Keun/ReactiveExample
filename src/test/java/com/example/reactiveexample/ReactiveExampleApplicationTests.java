1df7cec4e5a1abe5781e0edf6becf8caacf9bf57
U-keun Song <sukeun319@gmail.com>
2023-05-24 20:15:56 +0900

    Init : Sec.4 Reactive Programming & Project Reactor

---
+++ src/test/java/com/example/reactiveexample/ReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
