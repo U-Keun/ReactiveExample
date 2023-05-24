11b19e3286206a1646e44e47a3f2953746bb6bec
U-keun Song <sukeun319@gmail.com>
2023-05-25 02:42:20 +0900

    Flux with Range : range 메서드 사용

---
+++ src/main/java/com/example/reactiveexample/FluxRange.java
@@ -0,0 +1,12 @@
+package com.example.reactiveexample;
+
+import reactor.core.publisher.Flux;
+
+public class FluxRange {
+    public static void main(String[] args) {
+        // 5부터 숫자 10개
+        Flux<Integer> integerFlux = Flux.range(5, 10).log();
+
+        integerFlux.subscribe(System.out::println);
+    }
+}
