5f1fc437ea3f45ad77d0287548cf25b0c809cb80
U-keun Song <sukeun319@gmail.com>
2023-05-24 21:43:47 +0900

    List to Flux : 빈 리스트를 사용해도 에러가 없음!

--- src/main/java/com/example/reactiveexample/ListToFlux.java
+++ src/main/java/com/example/reactiveexample/ListToFlux.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class ListToFlux {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("우근", "현진", "승연", "정아");
+//        List<String> list = Arrays.asList("우근", "현진", "승연", "정아");
+        List<String> list = Arrays.asList();
+        // list = null은 안 됨!
 
         Flux<String> stringFlux = Flux.fromIterable(list).log();
 
