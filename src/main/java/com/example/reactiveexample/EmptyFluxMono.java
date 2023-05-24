d19f894e9a24f829f86fcf5928c5f39af180a7a1
U-keun Song <sukeun319@gmail.com>
2023-05-24 21:24:46 +0900

    Empty Flux and Mono : Mono 예 & ~.just(null)은 안 됨!

--- src/main/java/com/example/reactiveexample/EmptyFluxMono.java
+++ src/main/java/com/example/reactiveexample/EmptyFluxMono.java
@@ -1,11 +1,19 @@
 package com.example.reactiveexample;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class EmptyFluxMono {
     public static void main(String[] args) {
         Flux<?> emptyFlux = Flux.empty().log(); //  빈 Flux 객체
+//        Flux<?> emptyFlux = Flux.just(null).log(); // null을 집어넣는 것은 안됨(NullPointerException)
 
         emptyFlux.subscribe(System.out::println); // request(unbounded) -> onComplete()
+
+        // 빈 Mono 객체도 Flux와 똑같이 된다!
+        Mono<?> emptyMono = Mono.empty().log();
+//        Mono<?> emptyMono = Mono.just(null).log(); 이것도 안 됨.
+
+        emptyMono.subscribe(System.out::println);
     }
 }
