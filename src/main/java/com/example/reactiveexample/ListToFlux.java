8bad9f59d347333feebc8e810b138ecd0a0bea37
U-keun Song <sukeun319@gmail.com>
2023-05-24 21:42:15 +0900

    List to Flux : 리스트를 이용해서 Flux에 데이터 집어넣기(.fromIterable() 메서드 사용)

---
+++ src/main/java/com/example/reactiveexample/ListToFlux.java
@@ -0,0 +1,16 @@
+package com.example.reactiveexample;
+
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListToFlux {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("우근", "현진", "승연", "정아");
+
+        Flux<String> stringFlux = Flux.fromIterable(list).log();
+
+        stringFlux.subscribe(System.out::println);
+    }
+}
