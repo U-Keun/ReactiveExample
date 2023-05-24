490eca4c26c46db3d36f86b0e727c0770dc039b2
U-keun Song <sukeun319@gmail.com>
2023-05-25 03:05:20 +0900

    Map operation with Flux : map을 사용해도 데이터 하나를 보낼 때마다 map 내부 로직이 사용된다!

--- src/main/java/com/example/reactiveexample/FluxMap.java
+++ src/main/java/com/example/reactiveexample/FluxMap.java
@@ -6,13 +6,16 @@ public class FluxMap {
     public static void main(String[] args) {
         Flux<Order> orderFlux = Flux.just(
                 new Order(1L, 100.0),
-                new Order(1L, 100.0),
+                new Order(1L, 200.0),
                 new Order(3L, 300.0)
                 );
 
         Flux<Long> fluxLong = orderFlux
                 .filter(order -> order.getAmount() > 100.0)
-                .map(order -> order.getId())
+                .map(order -> {
+                    System.out.println("Inside Map");
+                    return order.getId();
+                })
                 .log();
 
         fluxLong.subscribe(System.out::println);
