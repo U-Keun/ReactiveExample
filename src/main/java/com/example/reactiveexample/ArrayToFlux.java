4d6f78bd67fb2da96eb041befbaf161862b9becf
U-keun Song <sukeun319@gmail.com>
2023-05-25 02:38:41 +0900

    Array to Flux : array 사용한 예제

---
+++ src/main/java/com/example/reactiveexample/ArrayToFlux.java
@@ -0,0 +1,15 @@
+package com.example.reactiveexample;
+
+import reactor.core.publisher.Flux;
+
+public class ArrayToFlux {
+    public static void main(String[] args) {
+        String[] array = new String[] {"우근", "성윤", "현진"};
+//        String[] array = new String[] {}; // 빈 배열도 들어갈 수 있다.
+//        String[] array = null; // NullPointerException
+
+        Flux<String> stringFlux = Flux.fromArray(array).log();
+
+        stringFlux.subscribe(System.out::println);
+    }
+}
